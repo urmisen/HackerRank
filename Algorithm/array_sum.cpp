c438278dc04f162fc967cae0924ee35ad69f4a16
urmisen <urmisen1202@gmail.com>
2019-10-04 02:26:28 +0600

    first commit

---
+++ Algorithm/array_sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+int n,sum=0;
+cin>>n;
+vector<int> arr(n);
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+    sum+=arr[i];
+}
+cout<<sum;
+return 0;
+}
---
+++ Algorithm/triplet_comparison.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector <int> solve(int a0,int a1,int a2,int b0,int b1,int b2){
+    vector<int> results(2);
+    results[0]=0;
+    results[1]=0;
+    int a[3],b[3];
+    a[0]=a0; a[1]=a1; a[2]=a2;
+    b[0]=b0; b[1]=b1; b[2]=b2;
+
+    for(int x=0;x<3;x++){
+        if(a[x]>b[x])
+            results[0]++;
+        else if(a[x]<b[x])
+            results[1]++;
+    }
+    return results;
+}
+
+int main(){
+    int a0,a1,a2;
+    cin>>a0>>a1>>a2;
+    int b0,b1,b2;
+    cin>>b0>>b1>>b2;
+
+    vector <int> result=solve(a0,a1,a2,b0,b1,b2);
+    for(ssize_t i=0;i<result.size();i++){
+        cout<<result[i]<<(i!=result.size()-1?" ":"");
+    }
+    cout<<endl;
+
+    return 0;
+}
